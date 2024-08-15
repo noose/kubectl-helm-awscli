2bd05e1beb8e59a0777ef45c83d60cac7cc83bcf
Pawel Kobylak <noose@noose.pl>
2024-08-15 21:33:18 +0200

    Missing file

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -xe
+
+: "${KUBE_CONFIG_DATA:="change me"}"
+: "${AWS_ACCESS_KEY_ID:="change me"}"
+: "${AWS_SECRET_ACCESS_KEY:="change me"}"
+
+# Extract the base64 encoded config data and write this to the KUBECONFIG
+echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
+
+export KUBECONFIG=/tmp/config
+
+bash -c "${*}"
